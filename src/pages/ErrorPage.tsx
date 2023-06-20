a97d969c723a2dc458fb2322340413d9cc1e8020
Cycle9898 <cycle9898@gmail.com>
2023-06-20 18:05:16 +0200

    feat: Error page complete integration

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function ErrorPage() {
-    return (
-        <p>Erreur 404, page non trouvée.</p>
-    );
+    return (<main className="main-error">
+        <h1>404</h1>
+
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+
+        <Link to="/">Retourner sur la page d’accueil</Link>
+    </main>);
 }
 
 export default ErrorPage;
